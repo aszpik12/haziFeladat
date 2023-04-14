16b9fbd6fa4ef8a38b1d21eae5b49fad3fc33f80
Petrovai Asztrik Örs <asztrikpetrovai@gmail.com>
2023-04-14 12:07:38 +0200

    info befejezve

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
 import { TERMEKLISTA } from "./adatok.js";
 
+let db = 0;
+let kosar = `kosár: `+ db;
+
 $(function () {
   const ARTICLE = $("article");
+  const FOCIM = $("#focim");
+  const FOOTER = $("footer");
+  FOCIM.html(`<h1>Vásárlói felület<h1>`);
   ARTICLE.append(divek);
   const MODELID = $("#myModal .modal-content");
   const INFOELEM = $(".info");
   const KOSARELEM = $(".kosar");
+  FOOTER.html(kosar);
 
   INFOELEM.on("click", function () {
     // modal on click programozása HIBÁS
-    let id= event.target.id;
-    console.log(id)
-MODELID.html(id)
+    let id = event.target.id;
+    console.log(id);
+    let modelbelselye = `
+    <div class="modal-header">
+      <h4 class="modal-title">${TERMEKLISTA[id].termek}</h4>
+      <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+    </div>
+
+    <!-- Modal body -->
+    <div class="modal-body">
+    <div class="card">
+    
+    <img src="${TERMEKLISTA[id].kep}" class="nagykep" alt="termék"><br>
+    <p class="card-body"> leírás: ${TERMEKLISTA[id].leiras}</p><br>
+    <div class="card-footer btn-group">
+    </div>
+    
+
+    <!-- Modal footer -->
+    <div class="modal-footer">
+      <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+    </div>`;
+    MODELID.html(modelbelselye);
   });
 
   KOSARELEM.on("click", function () {
-    alert("The paragraph was clicked.");
+    db++;
+    kosar = `kosár: `+ db;
+    FOOTER.html(kosar);
   });
 });
 
@@ -25,8 +54,9 @@ function divek() {
     //áru lusta legenerálása
     txt += `<div class="card col-lg-3 col-md-4 col-sm-6">`;
     txt += `<h2 class="card-header">${TERMEKLISTA[i].termek}</h2>`;
-    txt += `<img src="${TERMEKLISTA[i].kep}" alt="termék"><br>`;
+    txt += `<img src="${TERMEKLISTA[i].kep}" class="kiskep" alt="termék"><br>`;
     txt += `<p class="card-body"> ár: ${TERMEKLISTA[i].ar}</p><br>`;
+    txt += `<p class="card-body"> darab: ${TERMEKLISTA[i].darab}</p><br>`;
     txt += `<div class="card-footer btn-group">
           <button class="info btn btn-dark" data-bs-toggle="modal" data-bs-target="#myModal" id="${i}">Info</button>
           <button class="kosar btn btn-dark" ">Kosárba</button>
