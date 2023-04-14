b7a0c2624068bdcfaa0df0b6a0b2bd4fd44df46f
Petrovai Asztrik Örs <asztrikpetrovai@gmail.com>
2023-04-14 11:09:27 +0200

    modal id modositasa elkezdese

--- adatok.js
+++ adatok.js
@@ -1,7 +1,7 @@
 export const TERMEKLISTA = [
-    {termek: "laptop", darab: 0, ar: 72990, alapar: 72990, kep: ""},
-    {termek: "tablet", darab: 0, ar: 19999, alapar: 19999, kep: ""},
-    {termek: "xbox", darab: 0, ar: 134999, alapar: 134999, kep: ""},
-    {termek: "ps4", darab: 0, ar: 236999, alapar: 236999, kep: ""},
-    {termek: "asztali számítógép", darab: 0, ar: 139999, alapar: 139999, kep: ""}
+    {termek: "laptop", darab: 1, ar: 72990, alapar: 72990, kep: "", leiras: "könnyen hordozható laptop tökéletes a munka elvégzésére utazás közben!"},
+    {termek: "tablet", darab: 1, ar: 19999, alapar: 19999, kep: "", leiras: "nagy akumulátorral sokáig tart, hogy lemerűljön és könnyen lehet filmeket nézni rajta!"},
+    {termek: "xbox", darab: 1, ar: 134999, alapar: 134999, kep: "", leiras: "xbox pass-al elérhető hogy kipróbálhass több játékot!"},
+    {termek: "ps4", darab: 1, ar: 236999, alapar: 236999, kep: "", leiras: "MOST ps4 exclusive játékokkal!"},
+    {termek: "asztali számítógép", darab: 1, ar: 139999, alapar: 139999, kep: "", leiras: "mai játékokat akadozás nélkül elviszi legnagyobb grafikán!"}
 ]
--- main.js
+++ main.js
@@ -3,19 +3,61 @@ import { TERMEKLISTA } from "./adatok.js";
 $(function () {
   const ARTICLE = $("article");
   ARTICLE.append(divek);
+  const MODELID = $("#myModal .modal-content");
+  const INFOELEM = $(".info");
+  const KOSARELEM = $(".kosar");
+
+  INFOELEM.on("click", function () {
+    // modal on click programozása HIBÁS
+    let id= event.target.id;
+    console.log(id)
+MODELID.html(id)
+  });
+
+  KOSARELEM.on("click", function () {
+    alert("The paragraph was clicked.");
+  });
 });
 
 function divek() {
   let txt = "";
   for (let i = 0; i < TERMEKLISTA.length; i++) {
-    txt += `<div>`
-    txt += `<h2>${TERMEKLISTA[i].termek}</h2>`;
+    //áru lusta legenerálása
+    txt += `<div class="card col-lg-3 col-md-4 col-sm-6">`;
+    txt += `<h2 class="card-header">${TERMEKLISTA[i].termek}</h2>`;
     txt += `<img src="${TERMEKLISTA[i].kep}" alt="termék"><br>`;
-    txt += `<a> darab: ${TERMEKLISTA[i].darab}</a><br>`;
-    txt += `<a> ár: ${TERMEKLISTA[i].ar}</a><br >`;
-    txt += "<button><<</button><button>>></button>";
-    txt += `</div>`
+    txt += `<p class="card-body"> ár: ${TERMEKLISTA[i].ar}</p><br>`;
+    txt += `<div class="card-footer btn-group">
+          <button class="info btn btn-dark" data-bs-toggle="modal" data-bs-target="#myModal" id="${i}">Info</button>
+          <button class="kosar btn btn-dark" ">Kosárba</button>
+          </div>`;
+    txt += `</div>`;
   }
-  console.log(txt);
+
+  txt += `<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>`;
+
+  //console.log(txt);
+
   return txt;
 }
